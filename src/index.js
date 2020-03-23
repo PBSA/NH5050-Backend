7518b78adbcb686bcf899fcacf136d6b7337afb4
Alexander Dzhoganov <alexander.dzhoganov@gmail.com>
2020-03-23 14:43:02 +0200

    initial work on sequelize (#3)
    * initial work on sequelize
    
    * chore: backend server setup
    
    * fix: change cors origin to frontend url
    
    Co-authored-by: Prabhjot <prabhjot.narula@gmail.com>

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
-console.log('hello world');
+//import './db'
+import server from './server.js';
+
+(async () => {
+
+  try {
+    await new server().init();
+  } catch (err) {
+    console.log(err);
+  } finally {
+    console.log('server has been started');
+  }
+})();
