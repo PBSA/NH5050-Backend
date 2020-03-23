9c164b4063fc99e30118fb80b43c72bd4f1c1f8d
jotprabh1 <prabhjot.narula@gmail.com>
2020-03-23 10:24:39 -0300

    chore: apis to create, get and search users (#5)
    * chore: apis to create, get and search users
    
    * fix: removed foreach loop

---
+++ src/constants/profile.js
@@ -0,0 +1,9 @@
+export const userType = {
+  player: 'player',
+  seller: 'seller',
+  admin: 'admin'
+};
+export const status = {
+  inactive: 'inactive',
+  active: 'active'
+};
---
+++ src/validators/user.validator.js
@@ -0,0 +1,37 @@
+const Joi = require('./abstract/joi.form');
+const BaseValidator = require('./abstract/base.validator');
+
+class UserValidator extends BaseValidator {
+
+  /**
+   * @param {AppConfig} opts.config
+   * @param {UserRepository} opts.userRepository
+   * @param {BanHistoryRepository} opts.banHistoryRepository
+   */
+  constructor() {
+    super();
+
+    this.getUser = this.getUser.bind(this);
+    this.getUsers = this.getUsers.bind(this);
+  }
+
+  getUser() {
+    const querySchema = {
+      id: Joi.number().integer().required()
+    };
+
+    return this.validate(querySchema, null, (req, query) => query.id);
+  }
+
+  getUsers() {
+    const querySchema = {
+      email: Joi.string().email().required(),
+      mobile: Joi.string().regex(/^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/).required()
+    };
+
+    return this.validate(querySchema, null, (req, query) => query);
+  }
+
+}
+
+module.exports = UserValidator;
